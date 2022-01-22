81e698507e3fbef2fd49d3f9bea1c825d28b081e
takewi <gawi2k21@gmail.com>
2022-01-22 20:20:59 -0300

    🔥 Remove Td component

--- src/components/Td.tsx
+++
@@ -1,7 +0,0 @@
-
-export const Td = ({num}: {num: number}) => {
-    const handleOnclick = (num: number) => {
-        console.log(num)
-    }
-    return <td onClick={() => handleOnclick(num)}></td>
-}
